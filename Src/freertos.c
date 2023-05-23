0718901ec3cac7eb1cee91b995a91809c5942a33
Zelin Shen <zshen25@vt.edu>
2023-05-23 15:35:06 -0400

    debug fifo send

--- Src/freertos.c
+++ Src/freertos.c
@@ -208,11 +208,11 @@ void MX_FREERTOS_Init(void) {
 		//pc_communication_task saperate into 2
 //		osThreadDef(PCCOMMU, pc_communication_task, osPriorityNormal, 0, 256);
 //    pc_communication_task_handle = osThreadCreate(osThread(PCCOMMU), NULL);
-//		osThreadDef(EMBEDRX, embed_receive_Main_communication_task, osPriorityNormal, 0, 256);
-//    embed_receive_Main_communication_task_handle = osThreadCreate(osThread(EMBEDRX), NULL);
-//		
-//		osThreadDef(EMBEDTX, embed_send_communication_task, osPriorityNormal, 0, 256);
-//    embed_send_communication_task_handle = osThreadCreate(osThread(EMBEDTX), NULL);
+		osThreadDef(EMBEDRX, embed_receive_Main_communication_task, osPriorityNormal, 0, 256);
+    embed_receive_Main_communication_task_handle = osThreadCreate(osThread(EMBEDRX), NULL);
+		
+		osThreadDef(EMBEDTX, embed_send_communication_task, osPriorityNormal, 0, 256);
+    embed_send_communication_task_handle = osThreadCreate(osThread(EMBEDTX), NULL);
 
 		//Created task is either usb_task or usb_ano_task; Never both at the same time
     osThreadDef(USBTask, usb_task, osPriorityNormal, 0, 128);
