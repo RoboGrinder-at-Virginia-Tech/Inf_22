81813ea39438e68321c208a7e826f189b95174c4
Zelin Shen <zshen25@vt.edu>
2023-09-02 15:12:53 -0400

    comment change

--- Src/main.c
+++ Src/main.c
@@ -112,7 +112,7 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN 1Init */
 
   /* USER CODE END Init */
 
